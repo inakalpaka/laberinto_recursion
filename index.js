b3e2b7f6458e10b246a61ca710dbc4ee747fdbd6
inakalpaka <67813256+inakalpaka@users.noreply.github.com>
2023-08-28 20:16:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
-let mazeA = [
-    ["#", "#", "#", "#", "#"],
-    ["%", "0", "0", "0", "@"],
-    ["#", "#", "#", "0", "#"],
-    ["#", "#", "#", "0", "#"],
-    ["#", "#", "#", "#", "#"],
-];
-let mazeB = [
-    ["#", "0", "#", "#", "#"],
-    ["%", "0", "#", "0", "@"],
-    ["#", "0", "#", "0", "#"],
-    ["#", "0", "0", "0", "#"],
-    ["#", "#", "#", "#", "#"],
-];
 
-function laberinto(lab) {
-    if (lab[x][y] === "@") {
-        return true;
-    }
-    if (lab[x][y] === "#") {
-        return false;
-    }
-    if (x > 4 || x < 0||y > 4||y < 0) {
-        return false;
-    }
-    lab[x][y] = "+";
-    if(laberinto())
 
-}
+function resolverLaberinto(laberinto, x = 0, y = 0) {
+    if (x < 0 || y < 0 || x >= laberinto.length || y >= laberinto[0].length) {
+      return false;
+    }
+    const celda = laberinto[x][y];
+    if (celda === '#') {  // # es una pared
+      return false;
+    }
+  
+    if (celda === '$') {  // $ es el objetivo
+      return true;
+    }
+  
+    if (celda !== 'S' && celda !== '+') {
+      laberinto[x][y] = '+';  // + es parte recorrida del camino
+    }
+  
+    if (resolverLaberinto(laberinto, x - 1, y) ||  
+        resolverLaberinto(laberinto, x, y + 1) ||
+        resolverLaberinto(laberinto, x + 1, y) ||
+        resolverLaberinto(laberinto, x, y - 1)) {
+      return true;
+    }
+  
+    if (celda !== 'S') {
+      laberinto[x][y] = '.';  // . marca parte no recorrida del camino
+    }
+  
+    return false;
+  }
+  
+  // Ejemplo de laberinto representado como un arreglo de cadenas
+  const laberinto = [
+    "S######",  // S es el punto de salida
+    "#0#0###",  // # es una pared
+    "###0#0#",  // 0 es un camino
+    "#0#$#0#",  // $ es el objetivo
+    "#0#0###",  // 0 es un camino
+    "#######"
+  ];
+  
+  resolverLaberinto(laberinto);
+  
+  for (const fila of laberinto) {
+    console.log(fila);
+  }
