dd5c757f0cf3bce95aa6fa3aefd860a3b7e7a7de
inakalpaka <67813256+inakalpaka@users.noreply.github.com>
2023-08-29 09:49:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,61 @@
 
-
-function resolverLaberinto(laberinto, x = 0, y = 0) {
+function resolverLaberinto(laberinto, x = 1, y = 1) {
     if (x < 0 || y < 0 || x >= laberinto.length || y >= laberinto[0].length) {
-      return false;
+        return false;
     }
-    const celda = laberinto[x][y];
-    if (celda === '#') {  // # es una pared
-      return false;
+    if (laberinto[x][y] === "#") {
+        // # es una pared
+        return false;
     }
-  
-    if (celda === '$') {  // $ es el objetivo
-      return true;
+
+    if (laberinto[x][y] === "$") {
+        // $ es el objetivo
+        return true;
     }
-  
-    if (celda !== 'S' && celda !== '+') {
-      laberinto[x][y] = '+';  // + es parte recorrida del camino
+
+    if (laberinto[x][y] === "+") {
+        return false;
+    } else {
+        laberinto[x][y] = "+"; // + es la parte recorrida del camino
     }
-  
-    if (resolverLaberinto(laberinto, x - 1, y) ||  
+
+    if (
+        resolverLaberinto(laberinto, x - 1, y) ||
         resolverLaberinto(laberinto, x, y + 1) ||
         resolverLaberinto(laberinto, x + 1, y) ||
-        resolverLaberinto(laberinto, x, y - 1)) {
-      return true;
+        resolverLaberinto(laberinto, x, y - 1)
+    ) {
+        return true;
     }
-  
-    if (celda !== 'S') {
-      laberinto[x][y] = '.';  // . marca parte no recorrida del camino
-    }
-  
+    laberinto[x][y] = "0"; // . marca parte no recorrida del camino
     return false;
-  }
-  
-  // Ejemplo de laberinto representado como un arreglo de cadenas
-  const laberinto = [
-    "S######",  // S es el punto de salida
-    "#0#0###",  // # es una pared
-    "###0#0#",  // 0 es un camino
-    "#0#$#0#",  // $ es el objetivo
-    "#0#0###",  // 0 es un camino
-    "#######"
-  ];
-  
-  resolverLaberinto(laberinto);
-  
-  for (const fila of laberinto) {
+}
+
+// Ejemplo de laberinto representado como un arreglo de cadenas
+const laberinto = [
+    ["#", "#", "#", "#", "#", "#"],
+    ["#", "S", "0", "0", "0", "#"],
+    ["#", "0", "#", "#", "0", "#"],
+    ["#", "0", "#", "0", "0", "#"],
+    ["#", "0", "0", "#", "$", "#"],
+    ["#", "#", "#", "#", "#", "#"],
+];
+const laberinto2 = [
+    ["#", "#", "#", "#", "0", "#", "#", "#", "#", "#"],
+    ["0", "S", "0", "0", "0", "#", "0", "0", "0", "#"],
+    ["0", "#", "0", "#", "0", "#", "0", "#", "0", "#"],
+    ["#", "0", "0", "#", "0", "0", "0", "#", "0", "#"],
+    ["#", "0", "#", "#", "#", "#", "#", "#", "0", "#"],
+    ["#", "0", "0", "0", "0", "0", "0", "0", "0", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "#", "0", "#"],
+    ["#", "0", "0", "0", "0", "0", "0", "0", "0", "#"],
+    ["#", "0", "#", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", "0", "0", "0", "0", "0", "0", "0", "$", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
+];
+
+resolverLaberinto(laberinto2);
+
+for (const fila of laberinto2) {
     console.log(fila);
-  }
+}
